3a06bf1821b4350ef95498829ce718717cc8c430
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2024-02-22 12:45:36 +0545

    Added middleware folder and error and asyncerror classes to it

---
+++ middleware/catchAsyncError.js
@@ -0,0 +1,9 @@
+const catchAsyncErrors = (action) => async (req, res, next) => {
+  try {
+    await action(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default catchAsyncErrors;
