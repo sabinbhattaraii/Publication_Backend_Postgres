371d669144e58fb795ef4fb000b868e05f90f5c1
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2024-02-23 10:49:21 +0545

    Added route for checkout session

--- router/stripeRouter.js
+++ router/stripeRouter.js
@@ -8,5 +8,9 @@ stripeRouter
     .route("/create-payment-intent")
     .post(stripeController.createPaymentIntent)
 
+stripeRouter
+    .route("/create-checkout-session")
+    .post(stripeController.createCheckoutSession)
+
 
 export default stripeRouter;
