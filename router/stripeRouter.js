e96a3902ea52905ad90107554fee01c76d61c0b7
sabinbhattaraii <sabin.bhattarai2012@gmail.com>
2024-02-23 16:03:10 +0545

    Added a router for webhook

--- router/stripeRouter.js
+++ router/stripeRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { stripeController } from "../controller/index.js";
+import bodyParser from "body-parser";
 
 
 const stripeRouter = Router()
@@ -12,5 +13,10 @@ stripeRouter
     .route("/create-checkout-session")
     .post(stripeController.createCheckoutSession)
 
+stripeRouter
+    .use(bodyParser.raw({ type : 'application/json'}))
+    .route("/webhook")
+    .post(stripeController.handleWebhook)
 
+    
 export default stripeRouter;
